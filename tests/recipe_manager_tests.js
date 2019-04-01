a1ce9243081c61d0d3dcdac2669ea30cfeebb9c9
Ewa Trzemzalska <trzemson@gmail.com>
2019-04-01 19:03:03 +0200

    Adding some tests to API functions

--- tests/recipe_manager_tests.js
+++ tests/recipe_manager_tests.js
@@ -1,5 +1,42 @@
-const sum = require('../js/sum');
+const sum = require('../js/recipe_manager');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+let dataBase = {
+  recipes: [{
+    id: 4,
+    title: "Pasta alla Genovese"
+  },
+  {
+    id: 8,
+    title: "Pizza"
+  }
+  ]
+};
+
+
+test('getRecipes returns recipes', () => {
+  window.db = dataBase;
+  expect(getRecipes()).toBe([{
+    id: 4,
+    title: "Pasta alla Genovese"
+  }]);
+});
+
+test('getRecipe returns recipe by id ', () => {
+  window.db = dataBase;
+  expect(getRecipe(4).toBe({
+    id: 4,
+    title: "Pasta alla Genovese"
+  }));
+});
+
+test('getRecipe returns recipe by id ', () => {
+  window.db = dataBase;
+  expect(getRecipe(8).toBe({
+    id: 8,
+    title: "Pizza"
+  }));
 });
