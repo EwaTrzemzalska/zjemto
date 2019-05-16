3f2081d5ec671e9ff69ed6b0861684b5ef05d117
Ewa Trzemzalska <trzemson@gmail.com>
2019-05-16 17:36:30 +0200

    For looping over recipes by getRecipes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log('Hello World')
+
+const {
+  getRecipes,
+  getRecipe,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe
+} = require('../js/recipe_manager')
+
+// zrobic loopa ktory consolloguje recipesy
+// stworz funkcje ktorej jak przeslesz recipe to doda ja do listy na stronie
+
+const myRecipes = getRecipes()
+
+for (let i = 0; i < myRecipes.length; i++) {
+  console.log(myRecipes[i])
+}
